9b1908cde1d41585dab675d3591211febec57cf0
triptasoft <hasbullachalmughniy@gmail.com>
2023-03-27 12:35:50 +0700

    Beautify google button

--- resources/views/vendor/voyager/login.blade.php
+++ resources/views/vendor/voyager/login.blade.php
@@ -36,11 +36,9 @@
 
         <div style="clear:both"></div>
 
-        <a href="auth/google/redirect">
-        <button type="submit" class="btn btn-block login-button">
-                <span class="signingin hidden"><span class="voyager-refresh"></span> {{ __('voyager::login.loggingin') }}...</span>
-                <span class="signin">{{ __('voyager::generic.login') }}</span>
-            </button>
+        <a class="btn login-button" href="auth/google/redirect" role="button" style="text-transform:none">
+            <img width="15px" style="margin-bottom:3px; margin-right:5px" alt="Google sign-in" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/512px-Google_%22G%22_Logo.svg.png" />
+            Login with Google
         </a>
 
         <div style="clear:both"></div>
